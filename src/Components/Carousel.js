9326f7ccc978dde7733479ffaead3b02c6ee5901
sampath <sampathgoud451@gmail.com>
2024-07-11 12:07:07 +0530

    changes done now

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,70 +1,56 @@
-import React from 'react'
+import React from 'react';
 import turf from '../images/turf.jpeg';
 import slide1 from '../images/vkings1bc.jpeg';
-import slide2 from '../images/vkings2bc.jpeg';
 import arena from '../images/arena.jpeg';
 import add from '../images/add.jpeg';
+
 const Carousel = () => {
     return (
-        <>
-              <div className='carousel-container'>
-                <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
-                    <div className="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
-                    </div>
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img src={add} className="d-block w-100" alt="Slide 1" />
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>First slide label</h5>
-                                <p>Some representative placeholder content for the first slide.</p>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img src={turf} className="d-block w-100" alt="Slide 2" />
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>Second slide label</h5>
-                                <p>Some representative placeholder content for the second slide.</p>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img src={slide1} className="d-block w-100" alt="Slide 3" />
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>Third slide label</h5>
-                                <p>Some representative placeholder content for the third slide.</p>
-                            </div>
-                        </div>
-                        <div className="carousel-item active">
-                            <img src={arena} className="d-block w-100" alt="Slide 4" />
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>First slide label</h5>
-                                <p>Some representative placeholder content for the first slide.</p>
-                            </div>
-                        </div>
-                        <div className="carousel-item active">
-                            <img src={slide2} className="d-block w-100" alt="Slide 5" />
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>First slide label</h5>
-                                <p>Some representative placeholder content for the first slide.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
+        <div className='carousel-container'>
+            <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
                 </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={add} className="d-block w-100" alt="Slide 1" />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5><strong>Vkings sportz Arena</strong></h5>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <img src={turf} className="d-block w-100" alt="Slide 2" />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5><strong>Vkings sportz Arena</strong></h5>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <img src={slide1} className="d-block w-100" alt="Slide 3" />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5><strong>Vkings sportz Arena</strong></h5>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <img src={arena} className="d-block w-100" alt="Slide 4" />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5><strong>Vkings sportz Arena</strong></h5>
+                        </div>
+                    </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
-export default Carousel
+export default Carousel;
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='footer bg-warning'>
      <div className='text-center text-dark'>
-            &copy; {new Date().getFullYear()} Designed by Upsilon Studio.
+            &copy; {new Date().getFullYear()} Designed by Upsilon Studio|S@/^^/_gowd.
           </div>
     </footer>
   )
