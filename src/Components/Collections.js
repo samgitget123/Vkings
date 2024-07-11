16c627db9bc9847a97d293578377bf518489c362
sampath <sampathgoud451@gmail.com>
2024-07-11 09:28:05 +0530

    changes in collections component

--- src/Components/Collections.js
+++ src/Components/Collections.js
@@ -2,10 +2,15 @@ import React, { useState } from 'react';
 import image1 from '../images/chailogo.jpeg';
 import image2 from '../images/rishiskitchen.jpeg';
 import image3 from '../images/vr.jpeg';
+
 const Collections = () => {
   const [selectedImage, setSelectedImage] = useState(null);
 
-  const images = [image1, image2, image3]; // Add more images to this array
+  const images = [
+    { src: image1, alt: 'Chai Logo' },
+    { src: image2, alt: 'Rishi\'s Kitchen' },
+    { src: image3, alt: 'VR Experience' },
+  ]; // Add more images to this array
 
   const handleImageClick = (image) => {
     setSelectedImage(image);
@@ -21,10 +26,10 @@ const Collections = () => {
         {images.map((image, index) => (
           <div className='col-4 mb-4' key={index}>
             <img
-              src={image}
-              alt="Image"
+              src={image.src}
+              alt={image.alt}
               className='img-thumbnail gallery-image'
-              onClick={() => handleImageClick(image)}
+              onClick={() => handleImageClick(image.src)}
             />
           </div>
         ))}
